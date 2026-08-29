0ceb325e23d4aa7bab1f2c6bb36dd0291168ab20
suciayu28 <suci24si@mahasiswa.pcr.ac.id>
2026-08-29 20:38:05 +0700

    login dan register backend baru

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -50,7 +50,7 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect()->route('login');
     }
 
     /**
